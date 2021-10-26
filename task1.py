d70be5715dd8a5075d2f40192a9570e9d126ea34
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-10-26 22:23:25 +0530

    Add files via upload

--- task1.py
+++ task1.py
@@ -6,83 +6,93 @@ mat = int(input("Enter your Maths marks"))
 sci = int(input("Enter your Science marks"))
 soc = int(input("Enter your Social marks"))
 
-if((hin or eng or tel or mat or sci or soc) <= 35):
-    print("Fail")
-else:
+if(hin >= 35):
     if(hin >= 90 ):
-        print(hin, " / 100", "A*" )
+        print("Hindi   ",hin, " / 100", "A*",'p' )
     elif(hin >=80 ):
-        print(hin, " / 100", "A" )
+        print("Hindi   ",hin, " / 100", "A ",'p' )
     elif(hin >=70):
-        print(hin, " / 100", "B" )
+        print("Hindi   ",hin, " / 100", "B ",'p' )
     elif(hin >= 60):
-        print(hin, " / 100", "C" )
+        print("Hindi   ",hin, " / 100", "C ",'p' )
     elif(hin >=50):
-        print(hin, " / 100", "D" )
-    elif(hin >= 40):
-        print(hin, " / 100", "E" )
+        print("Hindi   ",hin, " / 100", "D ",'p' )
+    elif(hin >= 35):
+        print("Hindi   ",hin, " / 100", "E ","p" )
+else:
+    print("Hindi   ",hin, " / 100", "F ","F" )
 if(eng >= 35):
     if(eng >= 90 ):
-        print(eng, " / 100", "A*" )
+        print("English ",eng, " / 100", "A*","p" )
     elif(eng >=80 ):
-        print(eng, " / 100", "A" )
+        print("English ",eng, " / 100", "A ","p" )
     elif(eng >=70):
-        print(eng, " / 100", "B" )
+        print("English ",eng, " / 100", "B ","p" )
     elif(eng >= 60):
-        print(eng, " / 100", "C" )
+        print("English ",eng, " / 100", "C ","p" )
     elif(eng >=50):
-        print(eng, " / 100", "D" )
-    elif(eng >= 40):
-        print(eng, " / 100", "E" )
+        print("English ",eng, " / 100", "D ","p" )
+    elif(eng >= 35):
+        print("English ",eng, " / 100", "E ","p" )
+else:
+    print("English ",eng, " / 100", "F ","F" )
 if(tel >= 35):
     if(tel >= 90 ):
-         print(tel, " / 100", "A*" )
+         print("Telugu  ",tel, " / 100", "A*","p" )
     elif(tel >=80 ):
-         print(tel, " / 100", "A" )
+         print("Telugu  ",tel, " / 100", "A ","p" )
     elif(tel >=70):
-         print(tel, " / 100", "B" )
+         print("Telugu  ",tel, " / 100", "B ","p" )
     elif(tel >= 60):
-         print(tel, " / 100", "C" )
+         print("Telugu  ",tel, " / 100", "C ","p" )
     elif(tel >=50):
-         print(tel, " / 100", "D" )
-    elif(tel >= 40):
-         print(tel, " / 100", "E" )
+         print("Telugu  ",tel, " / 100", "D ","p" )
+    elif(tel >= 35):
+         print("Telugu  ",tel, " / 100", "E ","p" )
+else:
+    print("Telugu  ",tel, " / 100", "F ","F" )
 if(mat >= 35):
     if(mat >= 90 ):
-        print(mat, " / 100", "A*" )
+        print("Maths   ",mat, " / 100", "A*","p" )
     elif(mat >=80 ):
-        print(mat, " / 100", "A" )
+        print("Maths   ",mat, " / 100", "A ","p" )
     elif(mat >=70):
-        print(mat, " / 100", "B" )
+        print("Maths   ",mat, " / 100", "B ","p" )
     elif(mat >= 60):
-        print(mat, " / 100", "C" )
+        print("Maths   ",mat, " / 100", "C ","p" )
     elif(mat >=50):
-        print(mat, " / 100", "D" )
-    elif(mat >= 40):
-        print(mat, " / 100", "E" )
+        print("Maths   ",mat, " / 100", "D ","p" )
+    elif(mat >= 35):
+        print("Maths   ",mat, " / 100", "E ","p" )
+else:
+    print("Maths   ",mat, " / 100", "F ","F" )
 if(sci >= 35):
-    if(sci >= 90 ):
-        print(sci, " / 100", "A*" )
+    if("Science ",sci >= 90 ):
+        print("Science ",sci, " / 100", "A*","p" )
     elif(sci >=80 ):
-        print(sci, " / 100", "A" )
+        print("Science ",sci, " / 100", "A ","p" )
     elif(sci >=70):
-        print(sci, " / 100", "B" )
+        print("Science ",sci, " / 100", "B ","p" )
     elif(sci >= 60):
-        print(sci, " / 100", "C" )
+        print("Science ",sci, " / 100", "C ","p" )
     elif(sci >=50):
-        print(sci, " / 100", "D" )
-    elif(sci >= 40):
-        print(sci, " / 100", "E" )
+        print("Science ",sci, " / 100", "D ","p" )
+    elif(sci >= 35):
+        print("Science ",sci, " / 100", "E ","p" )
+else:
+    print("Science ",sci, " / 100", "F ","F" )
 if(soc >= 35):
     if(soc >= 90 ):
-         print(soc, " / 100", "A*" )
+         print("Social  ",soc, " / 100", "A*","p" )
     elif(soc >=80 ):
-         print(soc, " / 100", "A" )
+         print("Social  ",soc, " / 100", "A ","p" )
     elif(soc >=70):
-         print(soc, " / 100", "B" )
+         print("Social  ",soc, " / 100", "B ","p" )
     elif(soc >= 60):
-         print(soc, " / 100", "C" )
+         print("Social  ",soc, " / 100", "C ","p" )
     elif(soc >=50):
-         print(soc, " / 100", "D" )
-    elif(soc >= 40):
-         print(soc, " / 100", "E" )
+         print("Social  ",soc, " / 100", "D ","p" )
+    elif(soc >= 35):
+         print("Social  ",soc, " / 100", "E ","p" )
+else:
+    print("Social  ",soc, " / 100", "F ","F" )
